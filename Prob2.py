0b048842fb754fcd3f35d47b9d24c0f83fc47182
Matthew Gray <mwgdhh@umsystem.edu>
2020-02-25 17:16:25 -0600

    First upload

---
+++ Prob2.py
@@ -0,0 +1,17 @@
+print("Enter a whole number to be printed in Standard Factored Form: ")
+num = input(int)
+a = []
+while num % 2 == 0:
+    a.append(2)
+    num /= 2
+f = 3
+while f * f <= num:
+    if num % f == 0:
+        a.append(f)
+        num /= f
+    else:
+        f += 2
+if num != 1: a.append(num)
+# Only odd number is possible
+print (a)
+
